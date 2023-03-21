b30be6c9e1c6d4e3a61422cd5c73307a77c1e2b2
Albertoooooooo <103226207+Albertoooooooo@users.noreply.github.com>
2023-03-21 23:03:20 +1300

    tutorial
    yes

---
+++ trashcan/client/src/scenes/loginPage/index.jsx
@@ -0,0 +1,38 @@
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material"
+import Form from "./Form"
+
+const LoginPage = () => {
+    const theme = useTheme();
+    const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+    return <Box>
+        <Box 
+        width="100%" 
+        backgroundColor={theme.palette.background.alt} 
+        p="1rem 6%" 
+        textAlign="center"
+        >
+        <Typography
+          fontWeight="bold"
+          fontSize="32px"
+          color="primary"
+        >
+          Useless Web
+        </Typography>
+        </Box>
+
+        <Box
+            width={isNonMobileScreens ? "50%": "93%"}
+            p="2rem"
+            m="2rem auto"
+            borderRadius="1.5rem"
+            backgroundColor={theme.palette.background.alt}      
+        >
+            <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem"}}>
+                Welcome to The Useless Web, the Web of Useless Information.
+            </Typography>
+            <Form></Form>
+        </Box>
+    </Box>
+}
+
+export default LoginPage;
